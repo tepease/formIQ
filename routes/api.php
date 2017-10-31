9042c687b369bd9f6fca6ddb23c29ab935930c9c
Tristan <tepease@castlebranch.com>
2017-10-31 11:28:44 -0400

    frame out api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,25 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/forms', 'FormController@index');
+
+Route::post('/forms', 'FormConstroller@store');
+
+Route::get('/forms/{formId}', 'FormController@show');
+
+Route::patch('/forms/{formId}', 'FormController@update');
+
+Route::get('/forms/{formId}/questions', 'QuestionController@index');
+
+Route::get('/forms/{formId}/questions/{questionId}', 'QuestionController@show');
+
+
+
+Route::get('/users/{userId}/forms', 'Users/FormController@index');
+
+Route::get('/users/{userId}/forms/{formId}', 'Users/FormController@show');
+
+Route::get('/users/{userId}/forms/{formId}/sessions', 'SessionController@index');
+
+Route::get('/users/{userId}/forms/{formId}/sessions/{sessionId}', 'SessionController@show');
