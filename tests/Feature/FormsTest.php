133c66fb53bdc1bbfbbebaf1c44bc1b5f3055fc4
Tristan <tristan.e.pease@gmail.com>
2017-11-23 21:02:14 -0500

    restructure test methods for clarity

--- tests/Feature/FormsTest.php
+++ tests/Feature/FormsTest.php
@@ -2,20 +2,32 @@
 
 namespace Tests\Feature;
 
-use Tests\TestCase;
+use App\Form;
+use Tests\ApiTest;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class FormsTest extends TestCase
+class FormsTest extends ApiTest
 {
     /**
      * @return void
      */
-    public function stores_form()
+    public function fetches_form()
     {
-//        $this->makeForm();
-//
-//        $this->getJson();
-//
-        $this->();
+        $form = $this->makeForm();
+
+        $response = $this->getJson("api/forms/{$form->id}");
+
+        $response->assertStatus(200);
     }
+
+    private function makeForm($formFields = [])
+    {
+        $form = array_merge([
+            'anonymous' => $this->fake->boolean
+        ], $formFields);
+
+        return Form::create($form);
+    }
+
+
 }
